0fe8bbd6b344634896e73e6bf74c1ad142c619e6
MahmoodT <tahiramahmood1@hotmail.co.uk>
2018-03-02 14:33:14 +0000

    testing

--- Stage3_Verification_Tests/DealExtractorTest.cs
+++ Stage3_Verification_Tests/DealExtractorTest.cs
@@ -5,14 +5,18 @@ using Stage3_Verification;
 namespace Stage3_Verification_Tests
 {
     [TestClass]
-    class DealExtractorTest
+    public class DealExtractorTest
     {
         [TestMethod]
         public void ShouldPass_TheNumberOfRowsShouldBeTakenFromIDataExtractor()
         {
             var dataExtractorMock = new Mock<IDataExtractor>();
 
-            dataExtractorMock.Setup(data => data.);
+            dataExtractorMock.Setup(data => data.Extract(It.IsAny<string[]>()));
+            var dataExtract = new DataExtractor();
+            string[] testData= { "hello", "hi", "yes", "no" };
+            var extractions = dataExtract.Extract(testData);
+            Assert.AreSame(extractions, "4");
         }
     }
 }
