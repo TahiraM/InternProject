70bec3a0fe8aa89859e28a24a83998fa77a26509
MahmoodT <tahiramahmood1@hotmail.co.uk>
2018-03-07 10:15:05 +0000

    commit before i change everything

--- Stage3_Verification_Tests/DataExtractorTest.cs
+++ Stage3_Verification_Tests/DataExtractorTest.cs
@@ -1,21 +1,39 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
-using Stage3_Verification;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
+using NSubstitute.Routing.Handlers;
+using Stage3_Verification;
 
 namespace Stage3_Verification_Tests
 {
     [TestClass]
     public class DataExtractorTest
     {
+        private IDataExtractor _dataExtractor;
+        private IJsonConverter _dataToJsonConverter;
+        private IFileReader _fileReader;
+        private IFileWriter _fileWriter;
+
+
         [TestMethod]
         public void ShouldPass_TheNumberOfRowsShouldBeTakenFromIDataExtractor()
         {
-            var testingDataExtractor = new DataExtractor();
-            var dataExtractor = Substitute.For<IDataExtractor>();
-            dataExtractor.Extract
+            _dataExtractor = Substitute.For<IDataExtractor>();
+            _dataToJsonConverter = Substitute.For<IJsonConverter>();
+            _fileReader = Substitute.For<IFileReader>();
+            _fileWriter = Substitute.For<IFileWriter>();
+
+            var sut = new CsvToJsonConverter(_fileReader, _fileWriter, _dataExtractor,
+                                                _dataToJsonConverter);
 
 
+            
+            Assert.IsNotNull(_fileWriter.);
+
+          
         }
     }
+
+    
 }
