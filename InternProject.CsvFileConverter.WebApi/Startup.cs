e6d5678a78b05faf4d90deddbffc969559d5dc4b
MahmoodT <tahiramahmood1@hotmail.co.uk>
2018-06-19 16:11:45 +0100

    deleted web app

--- InternProject.CsvFileConverter.WebApi/Startup.cs
+++ InternProject.CsvFileConverter.WebApi/Startup.cs
@@ -1,21 +1,25 @@
-﻿using InternProject.CsvFileConverter.Library.Autofac;
+﻿using System.Web.Http;
+using InternProject.CsvFileConverter.Library.Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
-
+ 
 namespace InternProject.CsvFileConverter.WebApi
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
-        public string Url = "";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        public static void Register(HttpConfiguration config)
+        {
+            // New code
+            config.EnableCors();
+        }
 
         public Startup(IHostingEnvironment env)
         {
@@ -26,7 +30,7 @@ namespace InternProject.CsvFileConverter.WebApi
             Configuration = builder.Build();
         }
 
-        
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -34,9 +38,6 @@ namespace InternProject.CsvFileConverter.WebApi
             services.AddCoreServices(Configuration);
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Info {Title = "DealDatas", Version = "v1"}); });
             services.AddMvc();
-            services.AddCors(options =>
-                options.AddPolicy("AllowSpecificOrigin", builder => 
-                    builder.WithOrigins(Url).AllowAnyHeader()));
 
             AddDbServices(services, Configuration);
         }
@@ -48,7 +49,7 @@ namespace InternProject.CsvFileConverter.WebApi
 
             app.UseSwagger();
             app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "DealDatas"); });
-            app.UseCors("AllowSPecificOrigin");
+
             app.UseMvc();
         }
 
