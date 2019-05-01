9f0a56b8c2a5fc61ab05e6bbd12f23771d561267
Tahira Mahmood <Tahira.Mahmood@icgam.com>
2019-05-01 16:49:07 +0100

    tests editing

--- InternProject.CsvFileConverter.WebApi/Startup.cs
+++ InternProject.CsvFileConverter.WebApi/Startup.cs
@@ -10,7 +10,6 @@ namespace InternProject.CsvFileConverter.WebApi
     public class Startup
     {
         public string Url = "http://localhost:61686/";
-        public IConfiguration Configuration { get; }
 
         public Startup(IConfiguration configuration)
         {
@@ -26,17 +25,29 @@ namespace InternProject.CsvFileConverter.WebApi
             Configuration = builder.Build();
         }
 
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCoreServices(Configuration);
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Info { Title = "DealDatas", Version = "v1" }); });
             services.AddMvc();
+            services.AddCors(options => options.AddPolicy("AllowSpecificOrigin", builder =>
+            {
+                builder.WithOrigins(Url).AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            }));
+
             AddDbServices(services, Configuration);
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+            app.UseCors("AllowSpecificOrigin");
             app.UseSwagger();
             app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "DealDatas"); });
 
